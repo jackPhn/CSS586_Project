af7272a48fb98fe79440116aa887897139362cd1
Alex Kyllo <alex.kyllo@gmail.com>
2021-04-15 23:12:13 -0700

    add WavDataGenerator class

---
+++ musiclearn/processing.py
@@ -0,0 +1,83 @@
+"""processing.py
+Preprocessing audio data (.wav files) for analysis.
+"""
+import os
+from pathlib import Path
+import itertools
+import dotenv
+import random
+from tensorflow import keras
+from scipy.io import wavfile
+import numpy as np
+
+
+class WavDataGenerator(keras.utils.Sequence):
+    """A Keras Sequence for providing audio (.wav) file data in batches."""
+
+    def __init__(
+        self,
+        directory: os.PathLike,
+        batch_size: int = 32,
+        shuffle: bool = True,
+        max_len: int = 0,
+    ):
+        """Post-initialization"""
+        self.directory = Path(directory)
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        self.filenames = list(self.directory.glob("*.wav"))
+        self.max_len = max_len
+        self.on_epoch_end()
+
+    def __len__(self):
+        """The number of batches per epoch"""
+        return len(self.filenames) // self.batch_size
+
+    def _get_batch_filenames(self, index):
+        batch_start = index * self.batch_size
+        batch_end = (index + 1) * self.batch_size
+        indices = self.indices[batch_start:batch_end]
+        return [self.filenames[i] for i in indices]
+
+    def __getitem__(self, index):
+        """Get one batch of data as a numpy array, padded to length of the longest sequence."""
+        fnames = self._get_batch_filenames(index)
+        if self.max_len > 0:
+            wavs = [wavfile.read(f)[1][: self.max_len] for f in fnames]
+        else:
+            wavs = [wavfile.read(f)[1] for f in fnames]
+        return np.column_stack(list(itertools.zip_longest(*wavs, fillvalue=0)))
+
+    def on_epoch_end(self):
+        "Reshuffle indices after each epoch"
+        self.indices = np.arange(len(self.filenames))
+        if self.shuffle:
+            np.random.shuffle(self.indices)
+
+
+def test_batch_filenames():
+    """Test that it can create shuffled batches of filenames"""
+    dotenv.load_dotenv()
+    dir = Path(os.getenv("MUSIC_DATA_DIR")) / "train_data"
+    wdg = WavDataGenerator(dir, batch_size=2)
+    batch = wdg._get_batch_filenames(0)
+    assert Path(batch[0]).is_file
+    assert Path(batch[1]).is_file
+    # test it can shuffle
+    wdg.on_epoch_end()
+    batch1 = wdg._get_batch_filenames(0)
+    assert Path(batch1[0]).is_file
+    assert Path(batch1[1]).is_file
+    assert batch[0] != batch1[0]
+
+
+def test_batch_maxlen():
+    """Test that it retrieves data up to a max length"""
+    dotenv.load_dotenv()
+    dir = Path(os.getenv("MUSIC_DATA_DIR")) / "train_data"
+    # get 1 second of data per file
+    sample_rate = int(os.getenv("SAMPLE_RATE"))
+    batch_size = 2
+    wdg = WavDataGenerator(dir, batch_size=batch_size, max_len=sample_rate)
+    batch = wdg[0]
+    assert batch.shape == (batch_size, sample_rate)
