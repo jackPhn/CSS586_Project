6fb980a39ab2c4bb74f78844631ca48cbbb12776
Alex Kyllo <alex.kyllo@gmail.com>
2021-06-06 11:44:29 -0700

    add pitch class histogram

--- musiclearn/plotting.py
+++ musiclearn/plotting.py
@@ -6,7 +6,11 @@ import os
 
 import matplotlib
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
+import seaborn as sns
+
+from musiclearn.processing import REST, SUSTAIN
 
 
 def plot_learning_curves(history_file: os.PathLike, dest_path: os.PathLike):
@@ -25,8 +29,23 @@ def plot_learning_curves(history_file: os.PathLike, dest_path: os.PathLike):
     ax.set_ylim(0.0)
     ax.legend(loc="best")
     fig.tight_layout()
+    os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     plt.savefig(
         dest_path,
     )
 
     return (fig, ax)
+
+
+def plot_pitch_class_histogram(x: np.array, dest_path: os.PathLike, by_instrument: bool = True):
+    """Plot a histogram of pitches"""
+    if dest_path.endswith(".pgf"):
+        matplotlib.use("pgf")
+    xp = np.where(np.isin(x, [REST, SUSTAIN]), np.nan, x)
+    tracks = ["violin 1", "violin 2", "viola", "cello"]
+    df = pd.DataFrame(xp, columns=tracks)
+    ax = sns.histplot(data=df)
+    ax.set_xlabel("MIDI pitch value")
+    os.makedirs(os.path.dirname(dest_path), exist_ok=True)
+    plt.savefig(dest_path)
+    return ax
